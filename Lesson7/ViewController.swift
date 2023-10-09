10517d8ce1f744912928ae94dace5c1a704ef70c
glebychyasher <zobningleb01@icloud.com>
2023-10-09 20:42:06 +0300

    Initial Commit

---
+++ Lesson7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson7
+//
+//  Created by Глеб Зобнин on 09.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
